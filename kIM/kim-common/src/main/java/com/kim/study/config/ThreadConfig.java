9941bb7a60a088e1f3b7081abb9224d36d8c7e1d
huangjin <huangjin@carvingdata.cn>
2022-03-13 13:11:59 +0800

    新增线程池配置

---
+++ kIM/kim-common/src/main/java/com/kim/study/config/ThreadConfig.java
@@ -0,0 +1,44 @@
+package com.kim.study.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+/**
+ * @ClassName ThreadConfig
+ * @Description ThreadConfig配置类
+ * @Author KIM
+ * @Date 2022/3/8 16.38
+ * @Version 1.0
+ */
+/**
+ * 在插入方法中使用注解`@Async("taskExecutor")`进行异步调用，
+ * 使得该方法加入线程池运行，这个值为在是线程池中的方法名称，
+ * 需要与`@EnableAsync`一块使用，之前在创建线程池的类中已经开启
+ */
+
+@EnableAsync
+@Configuration
+public class ThreadConfig {
+
+    @Bean("taskExecutor")
+    public Executor asyncServiceExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        // 设置核心线程数
+        executor.setCorePoolSize(5);
+        // 设置最大线程数
+        executor.setMaxPoolSize(20);
+        //配置队列大小
+        executor.setQueueCapacity(Integer.MAX_VALUE);
+        // 设置线程活跃时间（秒）
+        executor.setKeepAliveSeconds(60);
+        // 等待所有任务结束后再关闭线程池
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        //执行初始化
+        executor.initialize();
+        return executor;
+    }
+}
--- kIM/kim-server/src/main/java/com/kim/study/service/Impl/IUserServiceImpl.java
+++ kIM/kim-server/src/main/java/com/kim/study/service/Impl/IUserServiceImpl.java
@@ -1,11 +1,14 @@
 package com.kim.study.service.Impl;
 
+import com.kim.study.config.test;
 import com.kim.study.entity.UserEntity;
 import com.kim.study.repository.IUserReporsitory;
 import com.kim.study.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.Executor;
 
 /**
  * @ClassName IUserServiceImpl
@@ -23,14 +26,18 @@ public class IUserServiceImpl implements IUserService {
     @Autowired
     private RedisTemplate redisTemplate;
 
+    @Autowired
+    private Executor executor;
+
 
     @Override
     public UserEntity getUserByUserName(String name) {
         UserEntity byName = iUserReporsitory.findByCode(name);
         String code = byName.getCode();
-
-        redisTemplate.opsForValue().set("woqu","hahahahahahha");
+        redisTemplate.opsForValue().set(code,"hahahahahahha");
         String o = (String) redisTemplate.opsForValue().get(byName.getCode());
+        //异步测试代码,同时可以写一个方法,上面加@Async("taskExecutor")注解实现异步操作
+        executor.execute(new test());
         System.out.println(o);
         return byName;
     }
