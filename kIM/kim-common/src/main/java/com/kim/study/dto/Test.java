1c4c3a55e3179385e9cb775e92f3896fbed9a15e
huangjin <huangjin@carvingdata.cn>
2022-06-23 16:37:51 +0800

    修改ftp连接服务器代码

---
+++ kIM/kim-common/src/main/java/com/kim/study/dto/Test.java
@@ -0,0 +1,18 @@
+package com.kim.study.dto;
+
+/**
+ * @ClassName Test
+ * @Description
+ * @Author KIM
+ * @Date 2022/6/23 16:37
+ * @Version 1.0
+ */
+public class Test {
+
+
+
+    public void test{
+        System.out.println("haha");
+    }
+
+}
