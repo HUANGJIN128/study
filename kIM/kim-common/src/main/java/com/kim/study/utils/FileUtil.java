94ef33976ff3efe437bd8a73c6afc9845256f96b
huangjin <huangjin@carvingdata.cn>
2022-07-25 11:02:08 +0800

    新增处理百万级别大数据excel导入工具类

--- kIM/kim-common/src/main/java/com/kim/study/utils/FileUtil.java
+++ kIM/kim-common/src/main/java/com/kim/study/utils/FileUtil.java
@@ -1,8 +1,8 @@
 package com.kim.study.utils;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
 
 /**
  * @ClassName FileUtil
@@ -11,26 +11,31 @@ import java.io.InputStream;
  * @Date 2022/7/22 17:31
  * @Version 1.0
  */
+@Slf4j
 public class FileUtil {
 
     /**
-     * 将InputStream写入本地文件
-     *
-     * @param filePath 本地绝对路径
-     * @param input       输入流
-     * @throws IOException IOException
+     *下载文件到服务器
+     * @param fis 输入流
+     * @param tempPath 下载绝对路径
+     * @throws IOException
      */
-    public static void writeToLocal(String filePath, InputStream input)
-            throws IOException {
-        int index;
-        byte[] bytes = new byte[1024];
-        FileOutputStream downloadFile = new FileOutputStream(filePath);
-        while ((index = input.read(bytes)) != -1) {
-            downloadFile.write(bytes, 0, index);
-            downloadFile.flush();
+    private void downLoadExcel(InputStream fis, String tempPath) throws IOException {
+        OutputStream outputStream=null;
+        try {
+            File file=new File(tempPath);
+            outputStream=new FileOutputStream(file);
+            int index;
+            byte[] bytes = new byte[1024];
+            while ((index = fis.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, index);
+            }
+        } catch (Exception e) {
+            log.error("uploadexcel to temp dir fail", e);
+        }finally {
+            fis.close();
+            outputStream.close();
         }
-        input.close();
-        downloadFile.close();
     }
 
 }
